77e94bea43975ab65765229f24b82554a672a433
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-26 00:07:47 +0600

    Mini E-commerce System

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class ShoppingCart {
-    private Product[] products;
-    private int numProducts;
+    private final List<Product> cartItems;
+
+    public ShoppingCart() {
+        this.cartItems = new ArrayList<>();
+    }
+
+    public void addToCart(Product product, int quantity) {
+        if (product.getQuantity() >= quantity) {
+            cartItems.add(new Product(product.getName(), product.getCategory(), product.getPrice(), quantity));
+            product.decreaseQuantity(quantity);
+        } else {
+            System.out.println("Not enough stock for " + product.getName());
+        }
+    }
+
+    public List<Product> getCartItems() {
+        return cartItems;
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Product product : cartItems) {
+            total += product.getPrice() * product.getQuantity();
+        }
+        return total;
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
 }
