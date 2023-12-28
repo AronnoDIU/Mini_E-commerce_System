87058ca4f4153e3e774e53f1ff8a1311d93a35b2
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-28 06:33:52 +0600

    Mini E-commerce System

--- src/PaymentProcessor.java
+++ src/PaymentProcessor.java
@@ -38,29 +38,29 @@ public class PaymentProcessor {
     private static void validatePaymentDetails(String creditCardNumber, String expirationDate, String cvv) {
         // Validate credit card number
         if (creditCardNumber.length() != 16) {
-            throw new IllegalArgumentException("Credit card number must be 16 digits.");
+            throw new IllegalArgumentException("Invalid credit card number. It must be 16 digits.");
         }
 
         // Validate expiration date
         String[] dateParts = expirationDate.split("/");
         if (dateParts.length != 2) {
-            throw new IllegalArgumentException("Expiration date must be in the format MM/YYYY.");
+            throw new IllegalArgumentException("Invalid expiration date format. It must be in the format MM/YYYY.");
         }
 
         int month = Integer.parseInt(dateParts[0]);
         int year = Integer.parseInt(dateParts[1]);
 
         if (month < 1 || month > 12) {
-            throw new IllegalArgumentException("Invalid month in expiration date.");
+            throw new IllegalArgumentException("Invalid month in expiration date. It must be between 1 and 12.");
         }
 
         if (year < 2021) {
-            throw new IllegalArgumentException("Invalid year in expiration date.");
+            throw new IllegalArgumentException("Invalid year in expiration date. It must be 2021 or later.");
         }
 
         // Validate CVV
         if (cvv.length() != 3) {
-            throw new IllegalArgumentException("CVV must be 3 digits.");
+            throw new IllegalArgumentException("Invalid CVV. It must be 3 digits.");
         }
     }
 }
