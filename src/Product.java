14d8c57844b317a0b59156f02c2d418c3ba76882
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-28 06:36:15 +0600

    Mini E-commerce System

--- src/Product.java
+++ src/Product.java
@@ -20,24 +20,35 @@ public class Product {
     }
 
     public static Product createProductFromConsoleInput(Scanner scanner) {
-        System.out.println("Enter product name:");
-        String name = scanner.nextLine();
+        try {
+            System.out.println("Enter product name:");
+            String name = scanner.nextLine();
 
-        System.out.println("Enter product description:");
-        String description = scanner.nextLine();
+            System.out.println("Enter product description:");
+            String description = scanner.nextLine();
 
-        System.out.println("Enter product price:");
-        double price = scanner.nextDouble();
-        scanner.nextLine(); // Consume the newline character
+            System.out.println("Enter product price:");
+            double price = scanner.nextDouble();
+            if (price < 0) {
+                throw new IllegalArgumentException("Price must be non-negative.");
+            }
+            scanner.nextLine(); // Consume the newline character
 
-        System.out.println("Enter product category:");
-        String category = scanner.nextLine().toUpperCase(); // Convert to uppercase
+            System.out.println("Enter product category:");
+            String categoryInput = scanner.nextLine().toUpperCase(); // Convert to uppercase
+            Category category = Category.valueOf(categoryInput); // Handle IllegalArgumentException
 
-        System.out.println("Enter product stock quantity:");
-        int stockQuantity = scanner.nextInt();
+            System.out.println("Enter product stock quantity:");
+            int stockQuantity = scanner.nextInt();
+            if (stockQuantity < 0) {
+                throw new IllegalArgumentException("Stock quantity must be non-negative.");
+            }
 
-        return new Product(null, name, description, price,
-                Category.valueOf(category), stockQuantity);
+            return new Product(null, name, description, price, category, stockQuantity);
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter valid product details.");
+            return null; // Indicate failure with null
+        }
     }
 
     // Getters
