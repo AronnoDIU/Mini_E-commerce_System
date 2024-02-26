87dc1aad2eb6497bc1f9f59dcd75619639caf256
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-02-26 19:23:02 +0600

    Update Authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication<T extends User> {
     private final Map<String, String> userCredentials;
     private String loggedInUser;
 
-    // Cons
+    // Co
     public Authentication(FileHandler fileHandler) {
         this.fileHandler = fileHandler;
 
