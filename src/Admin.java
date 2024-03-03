3d7e7310dddd1e246b06297dc40242ed20daa3ce
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-03-03 22:57:57 +0600

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -17,7 +17,7 @@ public class Admin extends User {
     }
 
 
-    // Update an existing product
+    // Update an existing
     public void updateProduct(Product product) {
         productCatalog.updateProduct(product);
         logSuccess();
