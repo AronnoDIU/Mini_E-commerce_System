9f10536c2d0c86675428956b4631de4524955ad0
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-03-15 20:41:25 +0600

    Update Authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -9,7 +9,7 @@ public class Authentication<T extends User> {
     public Authentication(FileHandler fileHandler) {
         this.fileHandler = fileHandler;
 
-        // Load ex
+        // Load e
         this.userCredentials = fileHandler.readUserCredentials();
         this.loggedInUser = null;
     }
