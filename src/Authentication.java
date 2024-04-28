9962ff26a938b3a4bbbda30102f424e6f10007ca
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-04-28 17:24:34 +0600

    Update Authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -18,7 +18,7 @@ public class Authentication<T extends User> {
             System.out.println("Username already exists. Please choose another username.");
         } else {
             userCredentials.put(username, password);
-            fileHandler.writeUserCredentials(userCredentials.toString()); // Save updated credentials to file
+            fileHandler.writeUserCredentials(userCredentials.toString()); // Save updated credentials to fil
             System.out.println("Registration successful. Welcome, " + username + "!");
         }
         return new User(username, password, username);
