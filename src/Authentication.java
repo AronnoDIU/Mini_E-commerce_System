fc7e6b8e0ef12b5a0c12bc4596f3c6ea40646f79
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-06-09 15:54:06 +0600

    Update Authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -24,7 +24,7 @@ public class Authentication<T extends User> {
         return new User(username, password, username);
     }
 
-    // User L
+    // User 
     public User login(String username, String password) {
         if (userCredentials.containsKey(username)) {
             if (userCredentials.get(username).equals(password)) {
