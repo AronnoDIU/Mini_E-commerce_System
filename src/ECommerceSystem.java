7cc9810ec976ace30603163bfc384e4d50aaef0f
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-07-17 13:13:35 +0600

    Update ECommerceSystem

--- src/ECommerceSystem.java
+++ src/ECommerceSystem.java
@@ -24,7 +24,7 @@ public class ECommerceSystem {
         this.userManager = new UserManager();
         this.productCatalog = new ProductCatalog();
         ordersList = new ArrayList<>();
-        currentUser = null; // No user is logged in init
+        currentUser = null; // No user is logged in ini
         this.scanner = new Scanner(System.in);
         loadInitialProducts();
         loadOrders();
