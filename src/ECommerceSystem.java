42d49d8da0c6921f34f0b3c05dc604cecf4f1b90
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-08-24 13:32:48 +0600

    Update ECommerceSystem

--- src/ECommerceSystem.java
+++ src/ECommerceSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ECommerceSystem {
     private final FileHandler fileHandler; // Add fileHandler declaration
     private final Authentication<User> authentication; // Add authentication declaration
-    private final Inventory inventory; // Add inventory declarati
+    private final Inventory inventory; // Add inventory declarat
     private final OrderManager orderManager;
     private final ProductManager productManager;
     private final UserManager userManager;
