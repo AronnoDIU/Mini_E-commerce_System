04a29832822ef7a67466e6dbe31941dc2eb900ac
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-11-11 11:51:39 +0600

    Update IAdminActions

--- src/IAdminActions.java
+++ src/IAdminActions.java
@@ -12,7 +12,7 @@ public interface IAdminActions {
     void viewProductStats();
 
     /**
-     * Manage users, including tasks such 
+     * Manage users, including tasks such
      */
     void manageUsers();
 }
